da47bb4ae93b588c1b460aedf3bd8da6e1deafe3
Siddhart <Siddhart_7@live.nl>
2022-03-31 13:49:25 +0200

    bruh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+
+    public static Student student;
+
     public static void main(String[] args) {
+
         //HIER KOMT ALLE LOGIC VOOR HET MENU
+
+        //login system maken die ene User object returned
+        //user object assignen naar Student
+
+        //loopen door elke gemaakte examen en dit ook assignen aan de Student met Examen Obejcts.
+            //user.getGemaaktExamen();
+
+        //Alle opties laten zien
+        //Interface laten zien
+
+        //vragen om input en het proces starten
+
+        Printer.Inloggen();
     }
 }
