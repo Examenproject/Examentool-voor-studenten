267c4b98b1633d0494f9e3c24f88f618654eafdc
Helloguis <94686622+Helloguis@users.noreply.github.com>
2022-04-04 18:45:18 +0200

    vragen netjes gemaakt en mogelijke switch case
    Als er een ding is die gamers leuk vinden is het natuurlijk de Nintendo Switch van Nintendo. Natuurlijk vind niet iedereen de Nintendo Switch van Nintendo leuk. Daarom gaan we bij de volgende meeting even kijken wat we het liefst in de code willen bewaren. De if statements of de Nintendo Switch. Verder liepen de vragen bij de output heel erg fuckywucky dus die heb ik minder fuckywucky gemaakt.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,7 +36,7 @@ public class Main {
 
         int studentNummer = 0;
 
-        if("aanmelden".equalsIgnoreCase(firstInput)){
+        if ("aanmelden".equalsIgnoreCase(firstInput)) {
             Printer.Registreren();
             String naamInput = scanner.nextLine();
             String achternaamInput = scanner.nextLine();
@@ -44,14 +44,14 @@ public class Main {
 
             studentNummer = JSON.addStudent(naamInput, achternaamInput, wachtwoordInput);
             student = JSON.getStudent(studentNummer);
-        }else{
+        } else {
             //input is a number
             studentNummer = Integer.parseInt(firstInput);
             String wachtwoordInput = scanner.nextLine();
 
             boolean isIngelogd = JSON.studentLogin(studentNummer, wachtwoordInput);
 
-            while(!isIngelogd){
+            while (!isIngelogd) {
                 System.out.println("Incorrecte validatie. Vul je studentnummer en wachtwoord opnieuw in.");
                 studentNummer = scanner.nextInt();
                 scanner.nextLine();
@@ -63,33 +63,61 @@ public class Main {
 
             student = JSON.getStudent(studentNummer);
         }
-        
+
         //print het hoofdmenu uit met alle opties
         int invoer = -1;
         Printer.Hoofdmenu();
-        while(invoer != 7) {
-            if(invoer != -1) {
+        while (invoer != 7) {
+            if (invoer != -1) {
                 Printer.simpelHoofdMenu();
             }
-            invoer = scanner.nextInt();
-            if (invoer == 1) {
-                Printer.Examenlijst();
-            } else if (invoer == 2) {
-                Printer.Studentenlijst();
-            } else if (invoer == 3) {
-                Examen.maakExamen();
-            } else if (invoer == 4) {
-                System.out.println("Welk examen wil je inzien?");
-                Printer.getGemaakteExamens(JSON.getGemaakteExamens(studentNummer));
-                int examenNummer = scanner.nextInt();
-                GemaaktExamen gemaaktExamen = new GemaaktExamen(examenNummer);
-                gemaaktExamen.examenInzien();
+            /**
+             * Voorbeeld van een switch case. Als het niets is halen we het morgen weg.
+             */
+           /*  switch (invoer = scanner.nextInt()) {
+                case 0:
+                    Printer.Examenlijst();
+                    break;
+                case 1:
+                    Printer.Studentenlijst();
+                    break;
+                case 2:
+                    Examen.maakExamen();
+                    break;
+                case 3:
+                    System.out.println("Welk examen wil je inzien?");
+                    Printer.getGemaakteExamens(JSON.getGemaakteExamens(studentNummer));
+                    int examenNummer = scanner.nextInt();
+                    GemaaktExamen gemaaktExamen = new GemaaktExamen(examenNummer);
+                    gemaaktExamen.examenInzien();
+                    break;
+                case 4:
+                    //lijst met beste studenten
+                    break;
+                case 5:
+                    //uitschrijven student
+                    break; */
 
-            } else if (invoer == 5) {
-                //lijst met beste studenten
-            } else if (invoer == 6){
-                //uitschrijven student
-            }
+
+        }
+        invoer = scanner.nextInt();
+        if (invoer == 1) {
+            Printer.Examenlijst();
+        } else if (invoer == 2) {
+            Printer.Studentenlijst();
+        } else if (invoer == 3) {
+            Examen.maakExamen();
+        } else if (invoer == 4) {
+            System.out.println("Welk examen wil je inzien?");
+            Printer.getGemaakteExamens(JSON.getGemaakteExamens(studentNummer));
+            int examenNummer = scanner.nextInt();
+            GemaaktExamen gemaaktExamen = new GemaaktExamen(examenNummer);
+            gemaaktExamen.examenInzien();
+
+        } else if (invoer == 5) {
+            //lijst met beste studenten
+        } else if (invoer == 6) {
+            //uitschrijven student
         }
     }
 }
