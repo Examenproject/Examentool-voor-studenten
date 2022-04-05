24b3f09bdea9a456d47907585137e2970c9e2082
JoeriGithub <101257742+JoeriGithub@users.noreply.github.com>
2022-04-05 12:58:50 +0200

    yo

--- src/main/java/ExamenAfnemen.java
+++ src/main/java/ExamenAfnemen.java
@@ -1,15 +1,76 @@
+import org.json.simple.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ExamenAfnemen {
-    String studentAntwoord;
-    public void stelVraag(ArrayList<String> vragenLijst){
+    Scanner scanner = new Scanner(System.in);
+    double goed = 0;
+    int fout = 0;
+    private ArrayList<Vraag> randomVragen;
+    boolean geslaagd;
+    int uniekExamenID;
+    int studentNummer;
 
-        for (int i = 0; i < vragenLijst.size(); i++) {
-            Printer.Vraag(vragenLijst.get(i));
+    public ExamenAfnemen(Student student) {
+        studentNummer = student.getStudentNummer();
+    }
+
+    public void maakExamen() {
+        System.out.println("Welk examen wil je maken?");
+        Printer.Examenlijst();
+        int input = scanner.nextInt();
+        uniekExamenID = input;
+        scanner.nextLine();
+
+
+        while (input > JSON.getExamList().size()) {
+            System.out.println("Verkeerde input, probeer opnieuw");
+            input = scanner.nextInt();
+            scanner.nextLine();
         }
 
-        
-        //Vraag.setAntwoord();
+        Examen examen = JSON.getExamenObject(input);
+        ArrayList<Vraag> examenVragen = examen.getVragen();
+
+        randomVragen = new ArrayList<Vraag>();
+
+        Collections.shuffle(examenVragen);
+
+        for (int i = 0; i < 10; i++) {
+            randomVragen.add(examenVragen.get(i));
+        }
+
+        stelVraag(randomVragen);
     }
+
+    public void stelVraag(ArrayList<Vraag> vragenLijst) {
+        String StudentAntwoord;
+
+        for (int i = 0; i < vragenLijst.size(); i++) {
+            Printer.Vraag(vragenLijst.get(i).getVraag(), i + 1);
+            StudentAntwoord = scanner.nextLine();
+
+            if (StudentAntwoord.equalsIgnoreCase(vragenLijst.get(i).getAntwoord())) {
+                goed++;
+            } else {
+                fout++;
+            }
+            vragenLijst.get(i).setStudentAntwoord(StudentAntwoord);
+
+        }
+        berekenCijfer(vragenLijst);
+
+
+    }
+
+    public void berekenCijfer(ArrayList<Vraag> vragenlijst) {
+        // 9 * S / L +N = Cijfer    <--- formule cijfer berekenen
+        Double cijfer = 9.0 * goed / randomVragen.size() + 1.0;
+        int gemaaktExamenID = JSON.saveGemaaktExamen(uniekExamenID, studentNummer, vragenlijst);
+        GemaaktExamen gemaaktExamen = JSON.getExamenAntwoordenObject(gemaaktExamenID);
+        Printer.Gehaald(gemaaktExamen.isGeslaagd(), cijfer);
+    }
+
 }
