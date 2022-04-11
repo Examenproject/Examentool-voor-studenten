12e5cefc2681d77ed38b9c65e00c6802ca80334d
Siddhart <Siddhart_7@live.nl>
2022-04-11 17:14:50 +0200

    Sike that's the wrong number!!

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -7,29 +7,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestClass {
-
-    @Test
-    public void testStudentNaam() {
-        String expected = "Karel";
-        String actual = JSON.getStudent(12345678).getNaam();
-        //  System.out.println(JSON.getStudent(12345678).getNaam()); overbodig aangezien de assertEquals al bevestigt dat de twee overeen komen.
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testStudentNummer() {
-        int expected = 12345678;
-        int actual = JSON.getStudent(12345678).getStudentNummer();
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testGemiddelde() {
-        double expected = 1.0;
-        double actual = JSON.getStudent(12345678).getGemiddelde();
-        Assert.assertEquals(expected, actual, 0.0001);
-    }
-
     @Test
     public void testStelVraag(){
         for(int i = 0; i<2; i++){
@@ -73,7 +50,7 @@ public class TestClass {
                 String wachtwoord = "1234";
                 System.setIn(new ByteArrayInputStream(wachtwoord.getBytes()));
                 Scanner scanner = new Scanner(System.in);
-                boolean isIngelogd = JSON.studentLogin(studentNummer, wachtwoord);
+                boolean isIngelogd = JSON.studentLogin(studentNummer, scanner.nextLine());
                 Assert.assertTrue(isIngelogd);
             }
             else if(i==1){
@@ -81,7 +58,7 @@ public class TestClass {
                 String wachtwoord = "fout";
                 System.setIn(new ByteArrayInputStream(wachtwoord.getBytes()));
                 Scanner scanner = new Scanner(System.in);
-                boolean isIngelogd = JSON.studentLogin(studentNummer, wachtwoord);
+                boolean isIngelogd = JSON.studentLogin(studentNummer, scanner.nextLine());
                 Assert.assertFalse(isIngelogd);
             }
             else if(i==2){
@@ -89,7 +66,7 @@ public class TestClass {
                 String wachtwoord = "1234";
                 System.setIn(new ByteArrayInputStream(wachtwoord.getBytes()));
                 Scanner scanner = new Scanner(System.in);
-                boolean isIngelogd = JSON.studentLogin(studentNummer, wachtwoord);
+                boolean isIngelogd = JSON.studentLogin(studentNummer, scanner.nextLine());
                 Assert.assertFalse(isIngelogd);
             }
             else{
@@ -97,44 +74,10 @@ public class TestClass {
                 String wachtwoord = "fout";
                 System.setIn(new ByteArrayInputStream(wachtwoord.getBytes()));
                 Scanner scanner = new Scanner(System.in);
-                boolean isIngelogd = JSON.studentLogin(studentNummer, wachtwoord);
+                boolean isIngelogd = JSON.studentLogin(studentNummer, scanner.nextLine());
                 Assert.assertFalse(isIngelogd);
             }
         }
     }
 
 }
-/**Kan examens niet ophalen **/
-    /*
-    @Test
-    public void testGemaaktExamens() {
-        ArrayList<GemaaktExamen> gemaaktExamen = JSON.getGemaakteExamens(12345678);
-        ArrayList<Integer> expected = new ArrayList<>();
-        expected.add(45247275);
-        expected.add(98137419);
-        ArrayList actual = JSON.getStudent(12345678).getExamens();
-        //  System.out.println(JSON.getStudent(12345678).getNaam()); overbodig aangezien de assertEquals al bevestigt dat de twee overeen komen.
-        Assert.assertEquals(expected, actual);
-    }
-
-}
-
-*/
-
-  /**Kan student niet ophalen. Idk why **/
-
-/* AanmeldTest werkt niet idk why
-            @Test
-    public void aanmeldTest(){
-                    Student student;
-                    int studentNummer;
-                    String naamInput = "Testy";
-                    String achternaamInput = "McTesty";
-                    String wachtwoordInput = "TestWachtwoord";
-
-                    studentNummer = JSON.addStudent(naamInput, achternaamInput, wachtwoordInput);
-                    Student actual = JSON.getStudent(studentNummer);
-                Assert.assertEquals(202213250,JSON.getStudent(studentNummer));
-                }
-
-            } */
